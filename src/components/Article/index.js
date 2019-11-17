77a8dc3237e694c61cbd2a709cc925468e98b38f
Денис Вильданов <denisvildanov@dvav.local>
2019-11-17 12:49:17 +0300

    Sass for article

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './index.scss';
+
 export const Article = ({ userImage, userName, postLink, title }) => {
     return (
-        <article>
-            <a href={postLink} target='_blank' rel='noopener noreferrer'>
+        <article className='article'>
+            <a href={postLink} target='_blank' rel='noopener noreferrer' className='article__link-wrapper'>
                 <img src={userImage} alt={`${userName}'s avatar`}/>
                 <h5>{userName}</h5>
                 <h3>{title}</h3>
