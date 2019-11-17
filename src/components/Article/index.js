3feba2e591583360c1a58189c9e529046e4057aa
Денис Вильданов <denisvildanov@dvav.local>
2019-11-17 13:16:36 +0300

    Html and css for article

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -6,9 +6,10 @@ export const Article = ({ userImage, userName, postLink, title }) => {
     return (
         <article className='article'>
             <a href={postLink} target='_blank' rel='noopener noreferrer' className='article__link-wrapper'>
-                <img src={userImage} alt={`${userName}'s avatar`}/>
-                <h5>{userName}</h5>
-                <h3>{title}</h3>
+                <div className='article__userinfo'>
+                    <img src={userImage} alt={`${userName}'s avatar`} className='article__user-avatar'/>
+                </div>
+                <h3 className='article__title'>{title}</h3>
             </a>
         </article>
     )
